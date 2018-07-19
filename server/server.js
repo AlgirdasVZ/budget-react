5b58668aae2ff2e10b00da455c3cf369a8989f59
Algirdas Vazgys <algirdas.vazgys@gmail.com>
2018-07-19 14:05:20 +1200

    Trying gzip on the server;

--- server/server.js
+++ server/server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const path = require('path');
+const compression = require('compression');
 const app = express();
 const publicPath = path.join(__dirname, '..', 'build');
 const port = process.env.PORT || 5000;
 
 app.use(express.static(publicPath));
+app.use(compression());
 
 app.get('/*', function (req, res) {
   res.sendFile(path.join(publicPath, 'index.html'));
